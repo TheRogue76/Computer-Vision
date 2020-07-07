9683f909d406d9d43878bafc40d6ebe3530a1ab9
habedi77 <hossein.abedi1998@gmail.com>
2020-07-07 14:56:50 +0430

    added dict for arco dynamic circles calcuate homography

--- Main4_ARUCO.py
+++ Main4_ARUCO.py
@@ -4,12 +4,15 @@ import cv2
 from cv2 import aruco
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-#import pandas as pd
+
+dict_arco = {
+    36 : 2,
+    33 : 3,
+    35 : 1,
+    34 : 0
+}
 
 frame = cv2.imread("examples/scene4.jpg")
-#plt.figure()
-#plt.imshow(frame)
-#plt.show()
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -17,25 +20,29 @@ parameters =  aruco.DetectorParameters_create()
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
+points = np.zeros((4,2),dtype=np.dtype('int32'))
 
-print(f"Index {0}, Arco:{ids[0]}")
-print(f"Index {1}, Arco:{ids[1]}")
-print(f"Index {2}, Arco:{ids[2]}")
-print(f"Index {3}, Arco:{ids[3]}")
-cv2.circle(frame_markers, (corners[0][0][2][0], corners[0][0][2][1]), 15, (0,0,255), -1)
-cv2.circle(frame_markers, (corners[1][0][3][0], corners[1][0][3][1]), 15, (0,255,0), -1)
-cv2.circle(frame_markers, (corners[2][0][1][0], corners[2][0][1][1]), 15, (255,0,0), -1)
-cv2.circle(frame_markers, (corners[3][0][0][0], corners[3][0][0][1]), 15, (0,0,0), -1)
-JJ = cv2.resize(frame_markers,(700,700))
-cv2.imshow("1",JJ)
+for i in range(4):
+    id = ids[i][0]
+    corn = dict_arco[id]
+    x = corners[i][0][corn][0]
+    y = corners[i][0][corn][1]
+    point = (x,y)
+    points[corn][0] = x
+    points[corn][1] = y
+    cv2.circle(frame_markers, point, 10, (0,0,0), -1)
 
-# plt.figure()
-# plt.imshow(frame_markers)
-# #for i in range(len(ids)):
-#     #c = corners[i][0]
-#     #plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
-# plt.legend()
-# plt.show()
+dest_points = np.array([
+    (0,0),
+    (500,0),
+    (500,500),
+    (0,500),
+])
+
+H, mask = cv2.findHomography(points, dest_points, cv2.RANSAC,4.0)
+print(f"H:: {H}")
+J = cv2.warpPerspective(frame_markers,H, (500,500))
+cv2.imshow("1",J)
 
 cv2.waitKey()
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
