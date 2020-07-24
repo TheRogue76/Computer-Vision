61eaa1fd823b9545c71c6ce750830457b6c74156
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2020-07-24 11:58:26 +0430

    cleaned up phase 2 and added prediction for 3 of the classes. found a threshold that is half decent

--- phaseTwo.py
+++ phaseTwo.py
@@ -1,20 +1,22 @@
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import cv2
 from cv2 import aruco
 import matplotlib.pyplot as plt
 from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Input
 from keras.models import Model
-import warnings
-warnings.filterwarnings('ignore')
+from keras.preprocessing.image import load_img, img_to_array
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+
 import keras
 import glob
 from tqdm import tqdm
 import os
 import numpy as np
 from matplotlib import pyplot as plt
-from keras.preprocessing.image import load_img, img_to_array
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint
+
 #model building processes
 def build_model(inputs):
   x = inputs
@@ -99,6 +101,13 @@ familyName = J_warped[325:367, 24:362]
 familyName = np.array_split(familyName, 8, axis=1)
 for i in range(8):
     cv2.imwrite("output/LN" + str(i) + ".jpg", familyName[i])
+    gray = cv2.cvtColor(familyName[i], cv2.COLOR_BGR2GRAY)
+    ret, gray = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY)
+    histg = cv2.calcHist([gray], [0], None, [256], [0, 256])
+    if histg[255] > histg[0]:
+        print("empty")
+    else:
+        print("not empty")
 
 phd = J_warped[394:411, 45:62]
 cv2.imwrite("output/PHD.jpg", phd)
@@ -109,6 +118,51 @@ cv2.imwrite("output/MS.jpg", masters)
 bachelor = J_warped[394:411, 280:296]
 cv2.imwrite("output/BS.jpg", bachelor)
 
+list_dataset_items = [
+	["no_0_", 0, "۰"],
+    ["no_1_", 1, "۱"],
+    ["no_2_", 2, "۲"],
+    ["no_3_", 3, "۳"],
+    ["no_4_", 4, "۴"],
+    ["no_5_", 5, "۵"],
+    ["no_6_", 6, "۶"],
+    ["no_7_", 7, "۷"],
+    ["no_8_", 8, "۸"],
+    ["no_9_", 9, "۹"],
+    ["al_al_", 10, "ا"],
+    ["al_be_", 11, "ب"],
+    ["al_pe_", 12, "پ"],
+    ["al_te_", 13, "ت"],
+    ["al_ss_", 14, "ث"],
+    ["al_jm_", 15, "ج"],
+    ["al_ch_", 16, "چ"],
+    ["al_hh_", 17, "ح"],
+    ["al_kh_", 18, "خ"],
+    ["al_dl_", 19, "د"],
+    ["al_zl_", 20, "ذ"],
+    ["al_rr_", 21, "ر"],
+    ["al_zz_", 22, "ز"],
+    ["al_jz_", 23, "ژ"],
+    ["al_sn_", 24, "س"],
+    ["al_shn_", 25, "ش"],
+    ["al_sd_", 26, "ص"],
+    ["al_zd_", 27, "ض"],
+    ["al_ta_", 28, "ط"],
+    ["al_za_", 29, "ظ"],
+    ["al_ay_", 30, "ع"],
+    ["al_ghy_", 31, "غ"],
+    ["al_fe_", 32, "ف"],
+    ["al_gh_", 33, "ق"],
+    ["al_kf_", 34, "ک"],
+    ["al_gf_", 35, "گ"],
+    ["al_lm_", 36, "ل"],
+    ["al_mm_", 37, "م"],
+    ["al_nn_", 38, "ن"],
+    ["al_vv_", 39, "و"],
+    ["al_he_", 40, "ه"],
+    ["al_ye_", 41, "ی"]
+]
+list_dataset_items = np.array(list_dataset_items)
 #compile and prepare model
 input = Input((28, 28, 1))
 model_alp = build_model(input)
@@ -120,37 +174,45 @@ model_num.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["acc"
 model_alp.load_weights("model_alp.h5")
 model_num.load_weights("model_no.h5")
 
-plt.figure(figsize=(3, 3))
-for i, test in enumerate(test_data):
+#predict student number
+for i, test in enumerate(studentNo):
     #prepare image for test (normalize to 0-1, and make batch)
     image = img_to_array(test) / 255.
     orig_img = image.copy()
     image = np.expand_dims(image, 0)
     #run prediction
-    predictions = model.predict(image)[0]
-    true_label = test_labels[i]
+    predictions = model_num.predict(image)[0]
     label = np.argmax(predictions)
-    if not working_digits:
-        #re-offset labels if working with alphabets
-        label = label+10
-        true_label = true_label+10
     #get predicted label character
     name_label = list_dataset_items[label][2]
     proba = np.max(predictions)
-    if label == true_label:
-        #go to next test if it was correct prediction
-        test_result_correct = test_result_correct+1
-        continue
-    else:
-        test_result_wrong = test_result_wrong+1
-    #diplay test image with the wrong label
-    output = cv2.resize(orig_img, (400, 400))
-    #optimized for showing image in Jupyter notebook
-    plt.subplot(1, 1, 1)
-    print(f"label {label}, true:{true_label}, {name_label}, {list_dataset_items[true_label][2]}")
-    plt.imshow(output, cmap="gray")
-    plt.axis("off")
-    plt.title("{}: {:.2f}%".format(name_label, proba * 100))
-    plt.show()
-print("Success rate: {:.2f}%".format(test_result_correct/test_result_total*100))
-print("Failure rate: {:.2f}%".format(test_result_wrong/test_result_total*100))
+
+#predict name
+for i, test in enumerate(name):
+    #prepare image for test (normalize to 0-1, and make batch)
+    image = img_to_array(test) / 255.
+    orig_img = image.copy()
+    image = np.expand_dims(image, 0)
+    #run prediction
+    predictions = model_alp.predict(image)[0]
+    label = np.argmax(predictions)
+    #re-offset labels because working with alphabets
+    label = label+10
+    #get predicted label character
+    name_label = list_dataset_items[label][2]
+    proba = np.max(predictions)
+    
+#predict family name
+for i, test in enumerate(familyName):
+    #prepare image for test (normalize to 0-1, and make batch)
+    image = img_to_array(test) / 255.
+    orig_img = image.copy()
+    image = np.expand_dims(image, 0)
+    #run prediction
+    predictions = model_alp.predict(image)[0]
+    label = np.argmax(predictions)
+    #re-offset labels because working with alphabets
+    label = label+10
+    #get predicted label character
+    name_label = list_dataset_items[label][2]
+    proba = np.max(predictions)
